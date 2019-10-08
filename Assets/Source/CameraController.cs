f271d04a42303e941443fd00a544c51cd1ad49d3
JCKolodziej <49226039+JCKolodziej@users.noreply.github.com>
2019-10-08 16:12:45 +0200

    Implement moveToPlayer and Go over map functions in Camera Controller

--- Assets/Source/CameraController.cs
+++ Assets/Source/CameraController.cs
@@ -7,8 +7,14 @@ public class CameraController : MonoBehaviour
     public Transform Player;
     public Vector3 Offset;
     public float FollowSpeed;
+    public bool follow;
+    float timeToReachEndPoint = 5;
+    float timeToReachPlayer = 2;
+    float timePast = 0;
+    public bool goToPlayer;
+    public bool timeOut;
+    public bool win;
 
-        
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +24,45 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (goToPlayer)
+        {
+            moveToPlayer();
+        }
 
+        if (follow)
+        {
+            FollowPlayer();
+        }
+        if (timeOut || win)
+        {
+            goToEndCameraPosition();
+        }
+    }
+
+    void FollowPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, Player.position + Offset, FollowSpeed);
+    }
+
+    void moveToPlayer()
+    {
+        var cameraPosition = transform.position;
+        var playerPosition = Player.position;
+        timePast += Time.deltaTime / timeToReachPlayer;
+        transform.position = Vector3.Lerp(cameraPosition, playerPosition, timePast);
+        if(Vector3.Distance(transform.position, Player.position) < 0.1f)
+        {
+            follow = true;
+            goToPlayer = false;
+            timePast = 0;
+        }
+        
+    }
+
+    void goToEndCameraPosition()
+    {
+        var endCameraPosition = GameObject.FindGameObjectWithTag("endcamera").transform.position; // here goes camera endposition tag
+        timePast += Time.deltaTime / timeToReachEndPoint;
+        transform.position = Vector3.Lerp(transform.position, endCameraPosition, timePast);
     }
 }
