adb442d08ba742ac87d459aa8f76b56bc052520b
Papcio Chmiel <pfaderewski@gmail.com>
2019-10-09 16:35:59 +0200

    2 additional scenes, ui ingame

---
+++ Assets/Source/TestUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+
+namespace Assets.Source
+{
+    class TestUI: MonoBehaviour
+    {
+        /*public GameObject Obj;*/
+
+        public TextMeshProUGUI TextObject;
+        public void OnButtonClick(string text)
+        {
+            //Obj.SetActive(!Obj.activeSelf);
+            TextObject.text = text;
+        }
+    }
+}
